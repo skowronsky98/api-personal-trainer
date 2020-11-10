db07aa5eabfa639dbfc13fa925ce9f2063219ffd
skowronsky <oskar.programming@gmail.com>
2020-11-10 21:31:10 +0100

    Controller and Service layers

--- src/main/java/com/trainer/api/controller/MealController.java
+++ src/main/java/com/trainer/api/controller/MealController.java
@@ -1,24 +1,27 @@
 package com.trainer.api.controller;
 
 import com.trainer.api.model.Meal;
-import com.trainer.api.repo.MealRepo;
+import com.trainer.api.service.MealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/api/trainer")
 public class MealController {
+
     @Autowired
-    private MealRepo mealRepo;
+    private MealService mealService;
 
     @GetMapping("/meal")
-    public Iterable<Meal> getAll(){
-        return mealRepo.findAll();
+    public Collection<Meal> getAll(){
+        return mealService.getMeals();
     }
 
     @PostMapping("/meal")
     public Meal add(@RequestBody Meal meal){
-        return mealRepo.save(meal);
+        return mealService.addMeal(meal);
     }
 
 }
--- src/main/java/com/trainer/api/dao/MealRepo.java
+++ src/main/java/com/trainer/api/dao/MealRepo.java
@@ -1,9 +1,12 @@
-package com.trainer.api.repo;
+package com.trainer.api.dao;
 
 import com.trainer.api.model.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface MealRepo extends JpaRepository<Meal, Long> {
+
 }
